4592412c1a8433dfb2874ddafe59f1245a7abe5c
Lasha-Ilashvili <ilashvililasha@gmail.com>
2023-12-10 16:46:50 +0400

    design update

--- app/src/main/java/com/example/vasestoreapp_hw12/fragment/VaseListFragment.kt
+++ app/src/main/java/com/example/vasestoreapp_hw12/fragment/VaseListFragment.kt
@@ -1,13 +1,14 @@
 package com.example.vasestoreapp_hw12.fragment
 
 import android.os.Bundle
-import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.widget.addTextChangedListener
+import androidx.appcompat.widget.SearchView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.example.vasestoreapp_hw12.R
 import com.example.vasestoreapp_hw12.adapter.VaseItemAdapter
 import com.example.vasestoreapp_hw12.data.Datasource
 import com.example.vasestoreapp_hw12.databinding.FragmentVaseListBinding
@@ -53,14 +54,27 @@ class VaseListFragment : Fragment() {
     }
 
     private fun setUpSearch() {
-        binding.etSearch.addTextChangedListener { title: Editable? ->
-            adapter.submitList(
-                title?.let { getSearchedVase(it) } ?: vaseList
-            )
-        }
+        binding.etSearch.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.gray))
+        binding.etSearch.setOnQueryTextListener(
+            object : SearchView.OnQueryTextListener {
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    adapter.submitList(
+                        newText?.let {
+                            getSearchedVase(it)
+                        }
+                    )
+
+                    return true
+                }
+
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    return false
+                }
+            }
+        )
     }
 
-    private fun getSearchedVase(searchedVase: Editable) =
+    private fun getSearchedVase(searchedVase: String) =
         vaseList.filter {
             it.title.contains(searchedVase, ignoreCase = true)
         }
